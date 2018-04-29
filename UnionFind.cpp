b406ecb150e7b21207bde411295e81fc3cb5885b
Željko Findak <zfindak@gmail.com>
2018-04-29 23:16:37 +0200

    Fixed bug

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -3,63 +3,73 @@ using namespace std;
 
 class UF {
 private:
-  int *parent;
-  int *size;
-  int componentsCount;
+    int *parent;
+    int *size;
+    int count;
 
 public:
-  UF(int n) {
-    parent = new int[n];
-    size = new int[n];
-    for (int i = 0; i < n; i++) {
-      parent[i] = i;
-      size[i] = 1;
+    UF(int N) {
+        parent = new int[N];
+        size = new int[N];
+        for (int i = 0; i < N; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+        count = N;
     }
-  }
 
-  ~UF() {
-    delete[] parent;
-    delete[] size;
-  }
-
-  int getCount() { return componentsCount; }
-
-  int findParent(int p) {
-    while (p != parent[p])
-      p = parent[p];
-    return p;
-  }
-
-  bool isConnected(int p, int q) { return findParent(p) == findParent(q); }
-
-  void unionNodes(int p, int q) {
-    int rootP = findParent(p);
-    int rootQ = findParent(q);
-    if (rootP == rootQ)
-      return;
-
-    if (size[rootP] < size[rootQ]) {
-      parent[rootP] = rootQ;
-    } else {
-      parent[rootQ] = rootP;
-      size[rootP] += size[rootQ];
+    ~UF() {
+        delete[] parent;
+        delete[] size;
     }
-    componentsCount--;
-  }
+
+    int getCount() {
+        return count;
+    }
+
+    int findRoot(int p) {
+        while (p != parent[p])
+            p = parent[p];
+        return p;
+    }
+
+    bool isConnected(int p, int q) {
+        return findRoot(p) == findRoot(q);
+    }
+
+    void unionNodes(int p, int q) {
+        int rootP = findRoot(p);
+        int rootQ = findRoot(q);
+        if (rootP == rootQ)
+            return;
+
+        if (size[rootP] < size[rootQ]) {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        } else {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+        count--;
+    }
 };
 
 
 int main() {
-  int n, p, q;
-  cin >> n;
-  UF uf = UF(n);
-  while (cin >> p) {
-    cin >> q;
-    if (uf.isConnected(p, q))
-      continue;
-    uf.unionNodes(p, q);
-    cout << p << " " << q << endl;
-  }
+    int N, p, q;
 
-  return 0;
+    cin >> N;
+    UF uf = UF(N);
+
+    while (cin >> p) {
+        cin >> q;
+        if (uf.isConnected(p, q))
+            continue;
+        uf.unionNodes(p, q);
+        cout << p << " " << q << endl;
+    }
+
+    cout << uf.getCount() << endl;
+
+    return 0;
 }
