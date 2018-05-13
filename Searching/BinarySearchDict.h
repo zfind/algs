bdec6a3aecde20d63c71cc773bd4abc44e6d9626
Željko Findak <zfindak@gmail.com>
2018-05-13 20:36:58 +0200

    Added binary search dictionary structure

---
+++ Searching/BinarySearchDict.h
@@ -0,0 +1,88 @@
+#ifndef BINARYSEARCHDICT_H
+#define BINARYSEARCHDICT_H
+
+
+#include <iostream>
+#include <cassert>
+
+
+template<typename Key, typename Value>
+class BinarySearchDict {
+private:
+
+    Key* keys;
+    Value* vals;
+    int N;
+
+public:
+
+    BinarySearchDict(int capacity) : N(0) {
+        keys = new Key[capacity];
+        vals = new Value[capacity];
+    }
+
+    ~BinarySearchDict() {
+        delete[] keys;
+        delete[] vals;
+    }
+
+    int size() {
+        return N;
+    }
+
+    bool isEmpty() {
+        return N == 0;
+    }
+
+    Value get(Key key) {
+        if (isEmpty()) return nullptr;
+        int idx = rank(key);
+        if (idx < N && keys[idx] == key) {
+            return vals[idx];
+        } else {
+            return nullptr;
+        }
+    }
+
+    void put(Key key, Value val) {
+        int idx = rank(key);
+        if (idx < N && keys[idx] == key) {
+            vals[idx] = val;
+            return;
+        }
+        for (int i=N; i>idx; i--) {
+            keys[i] = keys[i-1];
+            vals[i] = vals[i-1];
+        }
+        keys[idx] = key;
+        vals[idx] = val;
+        N++;
+    }
+
+    int rank(Key key) {
+        return rank(key, 0, N-1);
+    }
+
+    int rank(Key key, int lo, int hi) {
+        if (hi < lo) return lo;
+        int mid = lo + (hi - lo) / 2;
+        int cmp = key < keys[mid];
+        if (key < keys[mid]) {
+            return rank(key, lo, mid-1);
+        } else if (key > keys[mid]) {
+            return rank(key, mid+1, hi);
+        } else {
+            return mid;
+        }
+    }
+
+    void print() {
+        for (int i=0; i<N; i++) {
+            std::cout << "< " << keys[i] << " : " << vals[i] << " >" << std::endl;
+        }
+    }
+
+};
+
+
+#endif
